abd5796342de85f59c6c5363e58513d5ef4ce10c
Niloy Quazi <niloy940@gmail.com>
2019-11-16 20:43:47 +0600

    created migration, model, controllers and factories

---
+++ database/migrations/2019_11_16_124148_create_questions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('body');
+            $table->unsignedInteger('category_id');
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
