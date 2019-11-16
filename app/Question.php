a3bc7ac51b83c7db39e47d41eb3b611d34222f30
Niloy Quazi <niloy940@gmail.com>
2019-11-16 21:39:26 +0600

    creating relationships

--- app/Question.php
+++ app/Question.php
@@ -2,9 +2,25 @@
 
 namespace App;
 
+use App\Category;
+use App\Reply;
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
